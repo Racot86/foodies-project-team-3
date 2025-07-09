e118900ebcc44414ea7ad88e9e8640d0242d79ae
KatanovaLesya <katanovalesya@gmail.com>
2025-07-09 11:14:24 +0300

    +

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,32 @@
 import styles from "./Footer.module.css";
 import SocialLink from "../ui/SocialLink/SocialLink";
-import sprite from "../../../public/sprite.svg";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <p className={styles.footerText}>Footer</p>
+        <span className={styles.logo}>foodies</span>
+        <div className={styles.socials}>
+          <SocialLink to="https://facebook.com">
+            <svg width="20" height="20" className={styles.icon}>
+              <use xlinkHref="/sprite.svg#icon-facebook" />
+            </svg>
+          </SocialLink>
+          <SocialLink to="https://instagram.com">
+            <svg width="20" height="20">
+              <use xlinkHref="/sprite.svg#icon-instagram" />
+            </svg>
+        </SocialLink>
+        <SocialLink to="https://youtube.com">
+          <svg width="20" height="20">
+            <use xlinkHref="/sprite.svg#icon-youtube" />
+          </svg>
+        </SocialLink>
+        </div>
+      <div className={styles.line} />
+      <p className={styles.footerText}>
+        ©2024, Foodies. All rights reserved
+      </p>
       </div>
     </footer>
   );
