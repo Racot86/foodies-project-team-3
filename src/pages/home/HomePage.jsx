7ccb594f13b2c20588343643a19c0b776a9124ca
mariashubna <mariashubna@gmail.com>
2025-07-11 17:23:09 +0200

    scrollbar

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import {Outlet} from 'react-router-dom';
-import styles from './HomePage.module.css';
-import Page from '../../components/page/Page';
-import {Hero} from '@components/hero/Hero.jsx';
-import Testimonials from '@components/testimonials/Testimonials';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import styles from "./HomePage.module.css";
+import Page from "../../components/page/Page";
+import { Hero } from "@components/hero/Hero.jsx";
+import Testimonials from "@components/testimonials/Testimonials";
+import { AreaSelect } from "@/components/ui/Fields/FieldSelect/test";
 
 function HomePage() {
+  return (
+    <Page className={styles.homePage}>
+      <Hero />
 
-    return (
-        <Page className={styles.homePage}>
-            <Hero/>
+      {/* Example of using the responsive components */}
+      {/*<ResponsiveExample />*/}
+      <AreaSelect />
 
-            {/* Example of using the responsive components */}
-            {/*<ResponsiveExample />*/}
-
-            <Outlet/>
-            <Testimonials/>
-        </Page>
-    );
+      <Outlet />
+      <Testimonials />
+    </Page>
+  );
 }
 
 export default HomePage;
