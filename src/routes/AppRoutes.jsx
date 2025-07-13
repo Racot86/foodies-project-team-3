c0b434bfc6c14e0514e7627b10e934915043426e
mariashubna <mariashubna@gmail.com>
2025-07-13 02:24:15 +0200

    nothing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "@pages/home/HomePage";
 import AddRecipePage from "@pages/add-recipe/AddRecipePage";
 import ProfilePage from "@pages/profile/ProfilePage";
@@ -9,46 +9,43 @@ import Followers from "@pages/profile/components/Followers";
 import Following from "@pages/profile/components/Following";
 import DesignSystemPreview from "@pages/design-system/DesignSystemPreview";
 import NotFoundPage from "@pages/not-found/NotFoundPage";
-import {Layout} from "@components/layout/Layout";
+import { Layout } from "@components/layout/Layout";
 import RecipeCategories from "@pages/home/components/recipe-categories/RecepieCategories/RecipeCategories.jsx";
 import BrowseCategory from "@pages/home/components/recipe-categories/BrowseCategory/BrowseCategory.jsx";
 import RecipeDetails from "@pages/recipe-details/RecipeDetails";
-import {TestAuth} from "@components/testAuth";
+import { TestAuth } from "@components/testAuth";
 import PrivateRoute from "@components/PrivateRoute";
 
 export const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route element={<Layout/>}>
-                <Route path="/" element={<HomePage/>}>
-                    <Route index element={<RecipeCategories/>}/>
-                    <Route
-                        path="category/:categoryId"
-                        element={<BrowseCategory/>}
-                    />
-                </Route>
-                <Route path="/recipe-details/:recipeId" element={<RecipeDetails/>}/>
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<RecipeCategories />} />
+          <Route path="category/:categoryId" element={<BrowseCategory />} />
+        </Route>
+        <Route path="/recipe-details/:recipeId" element={<RecipeDetails />} />
 
-                {/* Protected routes that require authentication */}
-                <Route element={<PrivateRoute/>}>
-                    <Route path="/add-recipe" element={<AddRecipePage/>}/>
-                </Route>
+        {/* Protected routes that require authentication */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/add-recipe" element={<AddRecipePage />} />
+        </Route>
 
-              <Route element={<PrivateRoute/>}>
-                    <Route path="/profile" element={<ProfilePage/>}>
-                        <Route index element={<MyRecipes/>}/>
-                        <Route path="my-recipes" element={<MyRecipes/>}/>
-                        <Route path="favorites" element={<Favorites/>}/>
-                        <Route path="followers" element={<Followers/>}/>
-                        <Route path="following" element={<Following/>}/>
-                    </Route>
-                </Route>
-                <Route path="/design-system" element={<DesignSystemPreview/>}/>
-            </Route>
-            {/* Test Auth route for testing authentication functionality */}
-            <Route path="/test-auth" element={<TestAuth/>}/>
-            {/* Catch-all route for handling wrong/invalid URLs */}
-            <Route path="*" element={<NotFoundPage/>}/>
-        </Routes>
-    );
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<ProfilePage />}>
+            <Route index element={<MyRecipes />} />
+            <Route path="my-recipes" element={<MyRecipes />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="followers" element={<Followers />} />
+            <Route path="following" element={<Following />} />
+          </Route>
+        </Route>
+        <Route path="/design-system" element={<DesignSystemPreview />} />
+      </Route>
+      {/* Test Auth route for testing authentication functionality */}
+      <Route path="/test-auth" element={<TestAuth />} />
+      {/* Catch-all route for handling wrong/invalid URLs */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 };
