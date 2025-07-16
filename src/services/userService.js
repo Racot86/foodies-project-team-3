c9630ced8038cab22e33352c77b51319792604f1
Sergii <kryvkosergii@gmail.com>
2025-07-16 18:20:12 +0300

    user-profile draft

---
+++ src/services/userService.js
@@ -0,0 +1,18 @@
+import api from './api';
+
+export const userService = {
+
+    userDetails: async (userId) => {
+        return api.get(`/users/details/${userId}`);
+    },
+
+    uploadAvatar: async (file) => {
+        const formData = new FormData();
+        formData.append('avatar', file);
+        return api.patch(`/users/avatars`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+    }
+}
