42789be46c6077edbd71da795460b919f57ded24
Olena Deynega <suntrotter@gmail.com>
2025-07-17 17:48:00 +0300

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import RecipeDetails from '../src/pages/recipe-details/RecipeDetails';
-import SignInForm from '../src/components/signInForm/SignInForm'; 
-import SignUpForm from '../src/components/signUpForm/SignUpForm';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import { AppRoutes } from "@routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./styles/toastifyCustom.css";
 
-const AppRoutes = () => {
+export const App = () => {
   return (
-    <Routes>
-      <Route path="/signin" element={<SignInForm onClose={() => {}} />} />
-<Route path="/signup" element={<SignUpForm onClose={() => {}} />} />
-
-      <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-    </Routes>
+    <BrowserRouter>
+      <AppRoutes />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        toastClassName="toastify__custom"
+      />
+    </BrowserRouter>
   );
 };
-
-export default AppRoutes;
