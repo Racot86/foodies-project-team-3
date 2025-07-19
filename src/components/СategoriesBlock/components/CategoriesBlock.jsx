e0866ce6b4b0f175d121a012dde1168c26145e93
mariashubna <mariashubna@gmail.com>
2025-07-20 01:28:27 +0200

    category

--- src/components/СategoriesBlock/components/CategoriesBlock.jsx
+++ src/components/СategoriesBlock/components/CategoriesBlock.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import  Heading from "../../ui/Heading/Heading";
+import Heading from "../../ui/Heading/Heading";
 import CategoryList from "./CategoryList";
 import styles from "./CategoriesBlock.module.css";
 import { ResponsiveContainer } from "../../container/ResponsiveContainer";
 
 const CategoriesBlock = () => (
-  <ResponsiveContainer>
-    <div className={styles.section}>
-       <div className={styles.titlesubtitle}>
-          <div className={styles.titleBox}>
-            <Heading level={1} size="xl" weight="" align="left" className="">
-              Categories
-            </Heading>
-          </div>
-          <div className={styles.subtitleBox}>
-            <Heading level={2} size="xxs" weigh="" color="secondary" align="left" className="">
-              Discover a limitless world of culinary possibilities and enjoy exquisite recipes that combine taste, style and the warm atmosphere of the kitchen.
-            </Heading>
-          </div>
-        </div>
-      <CategoryList />
+  <div className={styles.section}>
+    <div className={styles.titlesubtitle}>
+      <div className={styles.titleBox}>
+        <Heading level={1} size="xl" weight="" align="left" className="">
+          Categories
+        </Heading>
       </div>
-  </ResponsiveContainer>
+      <div className={styles.subtitleBox}>
+        <Heading
+          level={2}
+          size="xxs"
+          weigh=""
+          color="secondary"
+          align="left"
+          className=""
+        >
+          Discover a limitless world of culinary possibilities and enjoy
+          exquisite recipes that combine taste, style and the warm atmosphere of
+          the kitchen.
+        </Heading>
+      </div>
+    </div>
+    <CategoryList />
+  </div>
 );
 
 export default CategoriesBlock;
-
