50d231418541dfb7fc29c784f589bcbff7ba15a3
Andrii <88sushilnikov@gmail.com>
2025-07-20 23:47:32 +0300

    replacing the buttonIcon logic with a navLink

--- src/pages/profile/components/userCard/UserCard.jsx
+++ src/pages/profile/components/userCard/UserCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Button, Heading, Text } from "@/components/ui";
 import { ButtonIcon } from "@/components/ui/ButtonIcon";
 import RecipePreviewList from "../recipePreviewList/RecipePreviewList";
@@ -9,7 +9,6 @@ import styles from "./UserCard.module.css";
 import { DEFAULT_AVATAR } from "@/services/api";
 
 const UserCard = ({ user, onButtonClick, tabType }) => {
-  const navigate = useNavigate();
   const firstName = user.name ? user.name.split(" ")[0] : "";
 
   const avatarUrl =
@@ -27,9 +26,7 @@ const UserCard = ({ user, onButtonClick, tabType }) => {
   const previewCount = getPreviewCount();
   const shouldShowPreview = previewCount > 0;
 
-  const handleNavigateToProfile = () => {
-    navigate(`/profile/${user.id}`);
-  };
+
 
   const handleButtonClick = () => {
     onButtonClick(user.id);
@@ -98,16 +95,11 @@ const UserCard = ({ user, onButtonClick, tabType }) => {
         </div>
       )}
 
-      <div className={styles.button}>
-        <ButtonIcon
-          onClick={handleNavigateToProfile}
-          className={styles.iconButton}
-        >
-          <svg className={styles.icon} width="16" height="16">
-            <use href={`${icons}#icon-arrow-up-right`} />
-          </svg>
-        </ButtonIcon>
-      </div>
+      <NavLink to={`/profile/${user.id}`} className={styles.navLink}>
+        <svg className={styles.icon} width="16" height="16">
+          <use href={`${icons}#icon-arrow-up-right`} />
+        </svg>
+      </NavLink>
     </div>
   );
 };
