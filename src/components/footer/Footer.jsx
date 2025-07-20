d8b69709353fcfdd1f4d6c3af2177d0af59cded9
mariashubna <mariashubna@gmail.com>
2025-07-20 18:48:30 +0200

    Footer bugs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,8 @@
-
 import styles from "../footer/Footer.module.css";
 import { SocialLink } from "../ui/SocialLink/SocialLink";
 
-import {RiInstagramFill} from "react-icons/ri";
-import { TfiFacebook } from "react-icons/tfi";
+import { RiInstagramFill } from "react-icons/ri";
+import { GrFacebookOption } from "react-icons/gr";
 import { FaYoutube } from "react-icons/fa6";
 import Logo from "../ui/Logo/Logo.jsx";
 
@@ -12,26 +11,31 @@ export const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.upperblock}>
         <div className={styles.content}>
-        <Logo />
+          <Logo />
+        </div>
+        <ul className={styles.socials}>
+          <li>
+            <SocialLink to="https://www.facebook.com/goITclub/">
+              <GrFacebookOption size={20} />
+            </SocialLink>
+          </li>
+          <li>
+            <SocialLink to="https://www.instagram.com/goitclub/">
+              <RiInstagramFill size={20} />
+            </SocialLink>
+          </li>
+          <li>
+            <SocialLink to="https://www.youtube.com/c/GoIT">
+              <FaYoutube size={20} />
+            </SocialLink>
+          </li>
+        </ul>
       </div>
-      <div className={styles.socials}>
-        <SocialLink to="https://facebook.com">
-          <TfiFacebook size={20} color="#050505"/>
-        </SocialLink>
-        <SocialLink to="https://instagram.com">
-          <RiInstagramFill size={20} color="#050505"/>
-        </SocialLink>
-        <SocialLink to="https://youtube.com">
-          <FaYoutube size={20} color="#050505"/>
-        </SocialLink>
-        </div>
-        </div>
       <div className={styles.divider} />
 
       <div className={styles.copyright}>
         ©2024, Foodies. All rights reserved
       </div>
-
     </footer>
   );
 };
